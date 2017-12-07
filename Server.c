c5a364d435a83b501bd8bdd8b09e4c0361ca72ea
Kelvin <kelvinliu1234@gmail.com>
2017-12-07 17:39:12 -0500

    server.c

---
+++ Server.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char **argv) 
+{
+	int ar=0;
+	if (argc != 3) 
+	{
+		printf("Usage: ./program -p <port>\n");
+		return 0;
+	} 
+
+	//int port = atoi(argv[2]);
+	int success;
+	char buffer[2560];
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	struct sockaddr_in serv_addr, cli_addr;
+
+	serv_addr.sin_port = htons(atoi(argv[2]));
+	serv_addr.sin_family = AF_INET;
+
+	bind(sockfd, (struct sockaddr *)  &serv_addr, sizeof(serv_addr));
+	listen(sockfd, 5);
+	int clilen = sizeof(cli_addr);
+
+	printf("LISTENING\n");
+
+	int incomingsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+
+	if (sockfd < 0) {
+		printf("Error opening socket!\n");
+		return 0;
+	}
+
+	printf("LISTENED\n");
+
+	while(1) {
+		bzero(buffer,2560);
+	    	success = read(incomingsockfd,buffer,2559);
+	    	if (success < 0) {
+			printf("ERROR READING\n");
+			return 0;
+		}
+		printf("Here is the message: %s\n",buffer);
+	}
+	return 0;
+}
